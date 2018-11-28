72f3024774cd71f1da9e17a867e0e7e49efc95a4
y-nakamura-y <y-nakamura@kowa-n.co.jp>
2018-11-28 16:43:31 +0900

    ドットインストール　MySQLの講座です。

---
+++ myapp21.sql
@@ -0,0 +1,12 @@
+drop table if exists posts;
+create table posts (
+  id int unsigned primary key auto_increment,
+  title varchar(255),
+  body text,
+  created datetime default current_timestamp,
+  updated datetime default current_timestamp on update current_timestamp
+);
+
+insert into posts (title, body) values ('title 1', 'body 1');
+insert into posts (title, body) values ('title 2', 'body 2');
+insert into posts (title, body) values ('title 3', 'body 3');
